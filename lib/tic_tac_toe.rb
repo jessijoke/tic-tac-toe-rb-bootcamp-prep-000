25880f6b6b6941ac2581bc3ae15c74befd51e86d
Jessica Bradham <jessijoke@yahoo.com>
2020-09-19 03:03:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,18 +57,6 @@ def current_player(board)
   else
     return "O"
   end
-  
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
