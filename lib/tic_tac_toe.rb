6f15919deec720cd53210fa5cdab451f94b5aee0
Jessica Bradham <jessijoke@yahoo.com>
2020-09-19 02:58:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,57 +8,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-def turn_count(board)
-  turns = 0
-  board.each do |space|
-    if space == "X" || space == "O"
-      turns += 1
-    end
-  end
-  return turns
-end
-
-def current_player(board)
-  turns = turn_count(board)
-  if turns % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
-end
-
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-# Define your WIN_COMBINATIONS constant
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
   [3,4,5], # Middle row
@@ -71,46 +20,3 @@ WIN_COMBINATIONS = [
   
 ]
 
-
-def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-    
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-    
-    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combination
-    end
-  end
-  return false
-end
-
-def full?(board)
-  board.all? {|space| space != " "}
-end
-
-def draw?(board)
-  if full?(board) && !won?(board)
-    return true
-  end
-end
-
-def over?(board)
-  if won?(board) || full?(board) || draw?(board)
-    return true
-  end
-end
-
-def winner(board)
-  if won?(board)
-    arr = won?(board)
-    index = arr[0]
-    return board[index]
-  else
-    return nil
-  end
-end
